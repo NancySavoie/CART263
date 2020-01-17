c1bc2bd442d15e9f19393e58199eede507897e6b
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-01-17 12:20:28 -0500

    A1: Created step 4 of the assignment

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -1,10 +1,13 @@
-// Assignment 1: Pixel Painter Pro
+// Assignment 1: Pixel Painter Pro++ (Based on Pippin Barr's Pixel Painter)
 // Nancy Savoie
 
-// With help from these websites:
+// With help from this website:
 // https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
 
 "use strict";
+
+// Global Variable
+let rotation = 0;
 
 window.onload = setup;
 
@@ -15,20 +18,44 @@ function setup() {
     pixel.setAttribute("class", "pixel");
     pixel.addEventListener("mouseover", paint);
     document.body.appendChild(pixel);
+    document.addEventListener('keydown', rotate); //For keydown
   }
 }
 
+// For the mouse over event listener
 function paint(e) {
   let pixel = e.target;
   pixel.style.backgroundColor = randomColor();
   setTimeout(resetPixel, 1000, pixel);
 }
 
+// Returns the color of the pixels to black
 function resetPixel(pixel) {
   pixel.style.backgroundColor = "black";
 }
 
+// Creates the random color
 function randomColor() {
   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   return randomColor;
 }
+
+// For the keydown event listener
+function rotate(buttonPressed) {
+  let pixels = document.getElementsByClassName('pixel');
+
+  // When the left arrow key is pressed, change the rotation to +1 clockwise
+  if (buttonPressed.keyCode === 37) {
+    rotation += 1;
+    for (let i = 0; i < pixels.length; i++) {
+      pixels[i].style.transform = `rotate(${rotation}deg)`;
+    }
+}
+    // When the right arrow key is pressed, change the rotation counter-clockwise
+    if (buttonPressed.keyCode === 39) {
+      rotation += -1;
+      for (let i = 0; i < pixels.length; i++) {
+        pixels[i].style.transform = `rotate(${rotation}deg)`;
+      }
+      }
+    }
