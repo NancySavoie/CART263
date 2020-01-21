3acc70a8d2eefdc8e7e26a3e89829443a81b8d5e
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-01-20 22:59:14 -0500

    A1: Added commentaries

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -11,6 +11,7 @@ let rotation = 0;
 
 window.onload = setup;
 
+// Function setup, creating pixel class
 function setup() {
   console.log("1,2,1,2, patate, poil");
   for (let i = 0; i < 1000; i++) {
@@ -29,12 +30,12 @@ function paint(e) {
   setTimeout(resetPixel, 1000, pixel);
 }
 
-// Returns the color of the pixels to black
+// Returns the color of the pixels to black, like the background
 function resetPixel(pixel) {
   pixel.style.backgroundColor = "black";
 }
 
-// Creates the random color
+// Creates the random color (Based on above-mentioned website)
 function randomColor() {
   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   return randomColor;
@@ -51,7 +52,7 @@ function rotate(buttonPressed) {
       pixels[i].style.transform = `rotate(${rotation}deg)`;
     }
   }
-  // When the right arrow key is pressed, change the rotation counter-clockwise
+  // When the right arrow key is pressed, change the rotation -1 counter-clockwise
   if (buttonPressed.keyCode === 39) {
     rotation += -1;
     for (let i = 0; i < pixels.length; i++) {
