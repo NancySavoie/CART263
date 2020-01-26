a3de668d5a8cbe9e3b05e6f74ab8b69ba49f2aee
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-01-26 01:39:59 -0500

    A2: Added mouse over function to the secret class and added comments

--- Assignments/Assignment2/js/script.js
+++ Assignments/Assignment2/js/script.js
@@ -14,8 +14,9 @@ const UPDATE_FREQUENCY = 500;
 
 // A place to store the jQuery selection of all spans
 let $spans;
+// Variables to track how many secrets were found
 let secretsFound = 0;
-let secretsTotal = 3;
+let secretsTotal = 5;
 
 // When the document is loaded we call the setup function
 $(document).ready(setup);
@@ -28,10 +29,14 @@ function setup() {
   $spans = $('span');
   // Set a click handler on the spans (so we know when they're clicked)
   $spans.on('click', spanClicked);
-// Set the score display text
-  $('#scoreDisplay').text("You found " + secretsFound + " out of " + secretsTotal + " secrets");
   // Set an interval of 500 milliseconds to update the state of the page
   setInterval(update, UPDATE_FREQUENCY);
+  // Set the score display text
+  $('#scoreDisplay').text("You found " + secretsFound + " out of " + secretsTotal + " secrets");
+// Calculate the total number of secrets on the page
+  secretsTotal = $('.secret').length;
+  // Event for "mouseover" to all the secrets, tirggers The "secretsReveal" function
+  $('.secret').on('mouseover', secretsReveal);
 };
 
 // spanClicked()
@@ -43,6 +48,13 @@ function spanClicked() {
   $(this).addClass('redacted');
 }
 
+// When the mouse goes over a secret, the found class, the secret, is highighted in yellow, thus revealed
+function secretsReveal() {
+  $(this).addClass('found'); // The CSS class "found"
+  $('.found').off('mouseover'); // Removes the mouse over event from the found element after the secret is found
+  secretsFound += 1; // Counter variable increased by one
+  $('#scoreDisplay').text();
+}
 // update()
 //
 // Update is called every 500 milliseconds and it updates all the spans on the page
