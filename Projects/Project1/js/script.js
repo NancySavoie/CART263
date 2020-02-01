712cb6694796d6adb62298fa41386de9aa12d925
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-01-31 22:55:11 -0500

    Pr1: Created assets and html for project 1

---
+++ Projects/Project1/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/********************************************************************
+
+Trivial Sisyphean Struggle
+Project 1 - Nancy Savoie
+
+Inspired by class exercises and examples.
+Images: Nancy Savoie and cretive commons (Vecteezy & Freepik)
+*********************************************************************/
