f52a872981e9aebf6d1b350fb39634013a75c894
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-02-01 22:49:26 -0500

    Pr1: Added dialog lines and fixed comments

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -26,11 +26,13 @@ let questions = [
   "Place the right vase on the according column - Sisyphus wouldn't quit.",
   "Place the right vase on the according column - Are you trying hard enough?",
   "Place the right vase on the according column - Working hard or hardly working?",
-  "Place the right vase on the according column - You call this working?",
+  "Place the right vase on the according column - You call this working?"
+  "Place the right vase on the according column - It shouldn't be too difficult."
+  "Place the right vase on the according column - Commitment is key...or is it?"
 ];
 
-// Load our disco Beethoven into a variable
-let music = new Audio('assets/sounds/backgroundMusic.mp3');
+// Load the background music.
+let music = new Audio("assets/sounds/backgroundMusic.mp3");
 
 // Variables to hold the columns and the vases
 let $column1;
@@ -51,65 +53,64 @@ let mouseMoves = 0;
 $(document).ready(setup);
 
 function setup() {
-
   if (mouseMoved) {
     music.play;
   }
 
-  $column1 = $('#column1');
+  $column1 = $("#column1");
   // Make it droppable
   $column1.droppable({
     // The drop option specifies a function to call when a drop is completed.
     drop: onDrop
   });
   // Repeat the same steps for the other columns.
-  $column2 = $('#column2');
+  $column2 = $("#column2");
   $column2.droppable({
     drop: onDrop
   });
 
-  $column3 = $('#column3');
+  $column3 = $("#column3");
   $column3.droppable({
     drop: onDrop
   });
 
-  $column4 = $('#column4');
+  $column4 = $("#column4");
   $column4.droppable({
     drop: onDrop
   });
 
-  $column5 = $('#column5');
+  $column5 = $("#column5");
   $column5.droppable({});
 
   // Get the vase element.
-  $vase1 = $('#vase1');
+  $vase1 = $("#vase1");
   // Make it draggable an revert to original position when released.
   $vase1.draggable({
     revert: true
   });
   // Repeat for the other vases.
-  $vase2 = $('#vase2');
+  $vase2 = $("#vase2");
   $vase2.draggable({
     revert: true
   });
 
-  $vase3 = $('#vase3');
+  $vase3 = $("#vase3");
   $vase3.draggable({
     revert: true
   });
 
-  $vase4 = $('#vase4');
+  $vase4 = $("#vase4");
   $vase4.draggable({
     revert: true
   });
 
-  $vase5 = $('#vase5');
+  $vase5 = $("#vase5");
   $vase5.draggable({
     revert: true
   });
 
   // Calls the mouseMoved function when mouse is moved.
-  $(document).on('mousemove', mouseMoved);
+  $(document).on("mousemove", mouseMoved);
   // Add a popup dialog after set amount of time.
   setTimeout(addDialog, INITIAL_DIALOG_DELAY);
 }
@@ -128,7 +129,6 @@ function mouseMoved() {
 
 // Randomly adds dialog to page.
 function addDialog() {
-
   // Creates a div to store variables.
   let $dialog = $(`<div></div>`).attr(`title`, `ασήμαντος αγώνας`);
   // Choose a random question text from the array
@@ -136,7 +136,7 @@ function addDialog() {
   // Add a p tag to the dialog div that contains the question text
   $dialog.append(`<p>${question}</p>`);
   // Finally, add the div to the page
-  $('body').append($dialog);
+  $("body").append($dialog);
 
   // The music starts with first popup
   handleMusic();
@@ -144,15 +144,15 @@ function addDialog() {
   // Transforms div into dialog with help from jQuery UI's.dialog() (https://api.jqueryui.com/dialog/#option-dialogClass).
   $dialog.dialog({
     buttons: {
-      "Ok": function() {
+      Ok: function() {
         $(this).dialog(`close`);
       },
-      "No": function() {
+      No: function() {
         $(this).dialog(`close`);
       }
     },
     close: closeDialog,
-    containment: 'body'
+    containment: "body"
   });
 
   // Use of .offset() on the .parent() of the dialog to give a random position + .height() and .width() to get the dimensions of elements on page.
@@ -164,7 +164,6 @@ function addDialog() {
 
 // Closes the dialog popup window.
 function closeDialog() {
-
   // Random time delay.
   let delay = randomInRange(MIN_DIALOG_DELAY_TIME, MAX_DIALOG_DELAY_TIME);
   // Set a timeout and add a new dialog after the delay.
@@ -173,16 +172,13 @@ function closeDialog() {
 
 // Returns a random number (Min & Max).
 function randomInRange(min, max) {
-  return min + (Math.random() * (max - min));
+  return min + Math.random() * (max - min);
 }
 
 // handleMusic function
 function handleMusic() {
-  music.loop
+  music.loop;
 }
 
+// OnDrop event
 function onDrop(event, ui) {}
-// We should "close the mouth" by changing its image
-// .attr() lets us change specific attributes on HTML element by specifying the attribute
-// and then what we want to set it to - in this case the 'src' attribute to the closed image
-//  $(this).attr('src', 'assets/images/mouth-closed.png');
