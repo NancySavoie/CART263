5386e52ad1d2592fdbb8ee6d3c03a5d5b029f258
Nancy Savoie <nancysavoiegiles@gmail.com>
2020-03-05 11:12:56 -0500

    P2: Trying to add different dialogue for the robot when he is fed internet garbage.

--- Projects/Project2/js/script.js
+++ Projects/Project2/js/script.js
@@ -23,17 +23,28 @@ let $trashFingerPuppet;
 let $trashHeadSwitch;
 let $trashRandomGenerator;
 let $trashRapeShirt;
-let timer // To eventually stop the animation
+let timer // Set to eventually stop the animation after the robot is fed internet garbage.
+// An array of voice messages for the robot.
+let voiceMessages = [
+  'This makes me sad',
+  'What is this?',
+  'System failing',
+  'Oh. My. God.',
+];
 
 $(document).ready(setup);
 
 function setup() {
 //Responsive voice with welcome message.
 responsiveVoice.speak(
-"Welcome human. I am from the future. The year 3021 to be exact. Please help me understand the decimation of human intelligence by feeding me your internet garbage",
+"Welcome human survivor. Click the mouse for some mood music. Now let us begin. I am from the future. The year 3021 to be exact. Please help me understand the decimation of human intelligence by feeding me your internet garbage",
 "UK English Female",
 {pitch: 0.5, rate: 1});
 $robot = $("#robot");
+$robot.droppable({
+// The drop option specifies a function to call when a drop is completed.
+    drop: onDrop
+  });
 // The trash elements
 $trashFingerPuppet = $("#trashFingerPuppet");
 $trashFingerPuppet.draggable();
@@ -43,10 +54,6 @@ $trashRandomGenerator = $("#trashRandomGenerator");
 $trashRandomGenerator.draggable();
 $trashRapeShirt = $("#trashRapeShirt");
 $trashRapeShirt.draggable();
-$robot.droppable({
-// The drop option specifies a function to call when a drop is completed.
-    drop: onDrop
-  });
 }
 
 $(document).one("click", mouseClick);
@@ -61,22 +68,21 @@ function onDrop(event, ui) {
   console.log("Drop");
   ui.draggable.remove();
   timer = setInterval(feedInternet, 250);
-  setTimeout(stop, 2000);
+  setTimeout(stop, 2000); // The animation stops after 2 seconds
 // Message for the Finger puppets
-  if ($trashFingerPuppet.draggable())
+  if ($("#trashFingerPuppet"))
   responsiveVoice.speak(
   "This makes me sad",
   "UK English Female",
   {pitch: 0.5, rate: 1});
- else {
 // Message for
-$trashRandomGenerator.draggable();
+ else if
+($("#trashHeadSwitch"))
 responsiveVoice.speak(
 "What is this",
 "UK English Female",
 {pitch: 0.5, rate: 1});
 }
-
 
 // Function that creates a small animation when the robot is fed internet garbage.
 function feedInternet(){
@@ -91,5 +97,4 @@ function feedInternet(){
 function stop(){
 clearInterval(timer);
 $robot.attr("src", "assets/images/robotNormal.png");
-}
 }
